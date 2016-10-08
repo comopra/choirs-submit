47067d3ce7a0ba40c7500803d3d159aa513b5d1e
tinycode2 <tinycode2@gmail.com>
2016-10-08 00:17:57 +0000

    starting to add model validation for json-ld documents

--- index.js
+++ index.js
@@ -5,30 +5,11 @@ const negotiate = require( "./src/negotiate" )( mediaTypes );
 
 module.exports.handler = ( event, context, callback ) => {
 
-    const body = "Hello from comopra. The time here is " + ( new Date().toLocaleTimeString() );
-    const data = { body };
-    const mediaType = negotiate( event );
-    switch( mediaType ) {
-        
-        case "text/html":
-            const view = "hello-world";
-            const sendHTML = gateway( "text/html", callback );
-            render( { view, data }, sendHTML );
-            break;
-            
-        case "application/json":
-            const sendJSON = gateway( "application/json", callback );
-            sendJSON( null, body );
-            break;
-            
-        default:
-            callback( new Error( 
-                
-                "No acceptable media-type available for: " + event.headers.Accept + 
-                ". Available types: " + mediaTypes
-                
-            ) );
-
-    }
+    const script = op => op
+        .shouldOnlyAccept( "application/ld+json" )
+        .shouldHaveSchema( "choir" );
+    // must be json-ld
+    // must have properties X, Y, Z
+    // and it must be stored
 
 };
---
+++ src/ld-validate.js
@@ -0,0 +1,153 @@
+
+function expandRangeValue( range, context ) {
+    
+    if ( !/\/\//.test( range ) ) {
+        
+        var pattern = /^(([^:]*):)(.*)$/;
+        var qualified = pattern.exec( range );
+        if ( qualified ) {
+
+            var alias = context[ qualified[ 2 ] ];
+            if ( alias ) { range = range.replace( pattern, alias + "$3" ); }
+
+        } else {
+            
+            var vocab = context[ "@vocab" ];
+            if ( vocab ) { range = vocab + range; }
+            
+        }
+        
+    }
+    return range;
+    
+}
+
+function resolveAndCheckForRequired( doc, key, spec ) {
+    
+    var resolved = doc.query( spec.ldquery );
+    if ( !resolved && spec.required ) { 
+        
+        throw new Error( "Missing: " + spec.ldquery + " for " + key );
+        
+    }
+    return resolved;
+
+}
+
+function validateSimpleValueType( key, value, spec ) {
+    
+    var type = spec.type;
+    if ( !type ) { return; }
+    var typeofValue = Array.isArray( value ) ? value.map( x => typeof x ) : typeof value;
+    var isTypeArrayMatch = Array.isArray( value ) && ~typeofValue.indexOf( type );
+    if ( !isTypeArrayMatch && ( typeofValue !== type ) ) {
+        
+        throw new Error( "Type of \"" + key + "\" is incorrect. Query was '" + spec.ldquery + "'. Expected: " + type + ". Actual: " + typeofValue );
+        
+    }
+    
+}
+
+function validateSimpleValueRange( key, value, spec ) {
+
+    var range = spec.range ? expandRangeValue( spec.range, context ) : null;
+    if ( range ) {
+        
+        var isArrayMatch = Array.isArray( value ) && ~value.indexOf( range );
+        if ( !isArrayMatch && ( value !== range ) ) {  
+
+            throw new Error( "Value for \"" + key + "\" out of range. Query was '" + spec.ldquery + "'. Expected: " + range + ". Actual: " + value );
+            
+        }
+        
+    }
+    
+}
+        
+function validateSimpleValue( doc, key, spec, context ) {
+
+    var value = resolveAndCheckForRequired( doc, key, spec );
+    if ( value ) {
+
+        if ( typeof value === "object" ) { value = value.json(); } // e.g. @type might return a querynode
+        validateSimpleValueType( key, value, spec );
+        validateSimpleValueRange( key, value, spec );
+
+    }
+    console.log( key, "is valid:", value );
+    
+}
+
+function resolveSchema( schemas, schemaName ) {
+    
+    var schema = typeof schemaName === "string" ? schemas[ schemaName ] : schemaName;
+    if ( !( schema && typeof schema === "object" ) ) {
+        
+        throw new Error( "Invalid schema: " + JSON.stringify( schemaName ) );
+        
+    }
+    return schema;
+    
+}
+
+function validateObjectValue( doc, key, spec, context, validateData ) {
+    
+    var resolved = resolveAndCheckForRequired( doc, key, spec );
+    if ( resolved ) {
+
+        console.log( "___", key, "___" );
+        validateData( spec.schema, resolved.json() ); 
+        
+    }
+    console.log( "___", key, "___" );
+
+}
+
+function validateDocumentAgainstSchema( context, schema, doc, validateData ) {
+    
+    for( var key in schema ) {
+
+        var spec = schema[ key ];
+        switch( spec.type ) {
+            
+            case "object":
+                
+                validateObjectValue( doc, key, spec, context, validateData );
+                break;
+                
+            default:
+                validateSimpleValue( doc, key, spec, context );
+                
+        }
+
+    }
+    
+}
+
+module.exports = function validationFactory( query, schemas, context ) {
+    
+    function validateData( schemaName, data ) {
+    
+        var schema = resolveSchema( schemas, schemaName );
+        var doc = query( data );
+        validateDocumentAgainstSchema( context, schema, doc, validateData );
+        
+    }
+
+    return function validate( schemaName, data, callback ) {
+    
+        try {
+            
+            validateData( schemaName, data );
+            
+        } catch( e ) {
+            
+            callback( e );
+            return;
+            
+        }
+        callback();
+        
+    };
+
+}
