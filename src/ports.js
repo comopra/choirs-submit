0f318c0e62fa456e05c7fc092ef8f175eccb2e8b
tinycode2 <tinycode2@gmail.com>
2016-10-14 23:36:31 +0000

    documentation

--- src/ports.js
+++ src/ports.js
@@ -1,7 +1,8 @@
+/* This allows injection of a mock */
 if ( global.AWS_SDK ) { console.warn( "Using global.AWS_SDK" ); }
+/* We use the AWS SDK to call S3 */
 const aws = global.AWS_SDK || require( "aws-sdk" );
-module.exports = {
-    
-    db: new aws.S3()
-    
-};
+/* our "database" */
+const db = new aws.S3();
+/* ports defined */
+module.exports = { db };
