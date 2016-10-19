f9f5033a1dbf0726edfa712921f2d343edd72637
tinycode2 <tinycode2@gmail.com>
2016-10-19 23:31:26 +0000

    moved test script out to operation-lambda

--- test/index.js
+++ test/index.js
@@ -1,33 +1,12 @@
-    
-    // mocks
-    const fakeConfig = { bucket: "the-bucket" };
-    require( "./mocks/aws-sdk" )( fakeConfig );
+const fakeConfig = { bucket: "the-bucket" };
+require( "./mocks/aws-sdk" )( fakeConfig );
 
-    // script to test
-    const script = require( "../src/script" );
-    
-    // testing operation
-    const StorageTestOperation = require( "./operations/StorageTestOperation" );
-    
-    // thing to test
-    const systemUnderTest = require( ".." );
-    
-    // how to fail the test run
-    const bail = up => (
-        
-        ur => codez => suck => { throw up; }
-        
-    )()()()()()()()()()()()()()();
-    
-    // outcomes
-    const pass = () => { console.log( "PASS" ); clearTimeout( pending ); };
-    const fail = e => process.nextTick( () => bail( e ) );
-    const timeout = () => { throw new Error( "Timed out" ); }
-    
-    // fire it up
-    new StorageTestOperation( systemUnderTest )
-        .execute( script )
-        .then( pass, fail );
-    
-    // start the timeout
-    const pending = setTimeout( timeout, 5000 );
+const test = require( "operation-lambda/test-operations/test" );
+
+test( {
+  
+    script: require( "../src/script" ),
+    TestOperation: require( "./operations/StorageTestOperation" ),
+    systemUnderTest: require( ".." )
+
+} );
