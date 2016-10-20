ad206d61459291f4f06f56bce05365313203bb34
tinycode2 <tinycode2@gmail.com>
2016-10-20 00:34:08 +0000

    newrelic unobtrusive config

--- newrelic.js
+++ newrelic.js
@@ -17,6 +17,7 @@ exports.config = {
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level: "trace"
+    level: "info",
+    filepath: "stdout"
   }
 }
